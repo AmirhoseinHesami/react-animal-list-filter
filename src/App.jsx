f7583216beae99d393e1a842d9275592fa6eedd6
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-05 02:40:39 +0330

    implement the app component and made some changes to the style.css file to make the app more polished and also in the app component a custome hook is defined to make search and fetch data easier

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    search(lastQuery);
+  }, []);
+
+  async function search(q) {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem("lastQuery", q);
+  }
+
+  return { search, animals };
+}
 
 function App() {
-  return <h1>hello</h1>;
+  const { animals, search } = useAnimalSearch();
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => search(e.target.value)}
+      />
+
+      {console.log("uo")}
+      <ul>
+        {animals.map((animal, i) => {
+          return <Animal key={animal.id} {...animal} index={i} />;
+        })}
+
+        {animals.length === 0 ? <p>No animals found</p> : null}
+      </ul>
+    </main>
+  );
+}
+
+function Animal({ type, name, age, index }) {
+  return (
+    <li>
+      {index}. <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
 }
 
 export default App;
